02490ed3da5637a9c8a462b2150a3189833b8171
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2024-08-23 19:22:34 -0500

    se agrega loader

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,9 @@ import { Html } from "@react-three/drei";
 export const Spinner = () => {
   return (
     <Html>
-      <div className="loader"></div>
+      <div className="lds-ripple">
+        <div></div>
+      </div>
     </Html>
   );
 };
